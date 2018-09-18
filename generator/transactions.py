222c9ea073d5d18d0fc21e4eb09c54ba9318d6a8
florimondmanca <florimond.manca@gmail.com>
2018-09-18 21:37:50 +0200

    fix maximum transaction amount

--- generator/transactions.py
+++ generator/transactions.py
@@ -7,12 +7,13 @@ account_chars: str = digits + ascii_letters
 
 
 def _random_account_id() -> str:
+    """Return a random account number made of 12 characters."""
     return ''.join(choices(account_chars, k=12))
 
 
 def _random_amount() -> float:
     """Return a random amount between 1.00 and 1000.00."""
-    return randint(100, 1000000) / 100
+    return randint(100, 100000) / 100
 
 
 def create_random_transaction() -> dict:
